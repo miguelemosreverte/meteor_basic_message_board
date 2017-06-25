2314d7edfc73b302389b2e5beb869fd7651e0f8e
miguelemosreverte <miguellemos95@gmail.com>
2017-06-24 21:10:49 -0300

    Added the input and messages templates to the html, which let the user write new messages and see them instantly printed on screen: Meteor uses an optimistic UI just for this, to allow for a better user experience. On main.js a function is called when the user clicks the Send Message button, which inserts the message on the client database.

--- main.js
+++ main.js
@@ -3,10 +3,35 @@ Messages = new Meteor.Collection("messages");
 if (Meteor.isClient) {
 
   Meteor.subscribe("messages");
+
+  Template.input.events({
+    'click .sendMsg': function(e) {
+       _sendMessage();
+    }
+  });
+
+  _sendMessage = function() {
+    var el = document.getElementById("msg");
+    Messages.insert({user: Meteor.user().username, msg: el.value, timestamps: new Date()});
+    el.value = "";
+    el.focus();
+  };
+
+
+  Template.messages.helpers({
+    messages: function() {
+      return Messages.find({}, {sort: {timestamps: -1}});
+    }
+  });
 }
 
 if (Meteor.isServer) {
 
+  Messages.allow({
+    insert: function (userId, doc) {
+      return (userId !== null);
+    }
+  });
   Meteor.publish("messages", function () {
     return Messages.find({}, {sort: {timestamps: -1}});
   });
